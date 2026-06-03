846994368067ddef348f807bd1606c3e53d9f09c
anusi <anusi@Anukriti>
2026-06-03 14:14:48 +0530

    initial commit 1

---
+++ testgit0103/src/main/java/com/git/test/testgit0103/App.java
@@ -0,0 +1,13 @@
+package com.git.test.testgit0103;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World in GIT" );
+    }
+}
